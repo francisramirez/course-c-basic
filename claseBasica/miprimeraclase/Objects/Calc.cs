c021f16da8098dfbb972047d807e40f2402194b5
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2023-10-07 15:55:09 -0400

    Ok.

---
+++ claseBasica/miprimeraclase/Objects/Calc.cs
@@ -0,0 +1,11 @@
+﻿namespace miprimeraclase.Objects
+{
+    public class Calc
+    {
+        public int Add(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+         
+    }
+}
--- claseBasica/miprimeraclase/Program.cs
+++ claseBasica/miprimeraclase/Program.cs
@@ -1,2 +1,28 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using miprimeraclase.Objects;
+
+string line = string.Empty;
+int num1 = 0;
+int num2 = 0;
+
+/*
+ *  Mi Comentario //
+
+ */
+Console.Write("Ingrese el valor de primer numero: ");
+line = Console.ReadLine();
+num1 = Convert.ToInt32(line);
+
+
+Console.Write("Ingrese el valor del segundo numero: ");
+line = Console.ReadLine();
+num2 = Convert.ToInt32(line);
+
+
+Calc calc = new Calc();
+
+var result = calc.Add(num1, num2);
+
+Console.WriteLine($"Este es el resultado: { result }");
+
+Console.ReadLine();
